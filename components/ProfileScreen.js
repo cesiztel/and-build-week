d2189588de7437677c1aeea46870eb203ce8e8e5
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2021-06-17 15:16:55 +0200

    update dependencies + add react icon

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { View, Text, Image, Button, SafeAreaView } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
 
 const styles = {
-  coverPhoto: {
+  uploadPhotoIcon: {
+    color: "black",
+  },
+  profilePhoto: {
     width: 120,
     height: 120,
     resizeMode: "cover",
     borderRadius: "50%",
+    boxShadow: "rgb(255 50 60 / 60%) 9px 8px 25px",
   },
   mainContainer: {
     display: "flex",
@@ -55,11 +61,14 @@ const ProfileScreen = () => {
       <View style={styles.mainContainer}>
         <View style={styles.introContainer}>
           <Image
-            style={styles.coverPhoto}
+            style={styles.profilePhoto}
             source={{
               uri: "https://images.unsplash.com/photo-1612061279301-c1bd3a484fcf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
             }}
           />
+          <View style={styles.uploadProfilePhoto}>
+            <Icon name="rocket" size={30} color="#900" style={styles.uploadPhotoIcon} />
+          </View>
           <Text style={styles.userName}>@Grace Leah</Text>
           <Text style={styles.introduction}>
             People's Lead and Cat Fanatic. <br />I love cocktails and long walks
