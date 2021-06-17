0f222e40070fa13e422e98a912ae2e32b2bc7211
shubhadali <85987719+shubhadali@users.noreply.github.com>
2021-06-17 12:47:36 +0200

    feat: add some changes

--- App.js
+++ App.js
@@ -1,26 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, Image, Button, SafeAreaView } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './components/HomeScreen';
+import ExperienceScreen from './components/ExperienceScreen';
+import HomeScreen from './components/HomeScreen';
+import ProfileScreen from './components/ProfileScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
- 
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Whats up ANDis!</Text>
-      <Image source={{
-        width: 300,
-        height: 100,
-        
-        uri: "https://commercetools.com/wp-content/uploads/2021/03/and-digital-logo.png"}} resizeMode="contain"/>
-        <Button  title="Click Me" color="red" onPress={() => alert("Button Clicked")}/>
-
-
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+      </Stack.Navigator>
+      
+    </NavigationContainer>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -29,4 +34,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  home: {
+    flex: 1,
+  }
 });
---
+++ components/HomeScreen.js
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { StyleSheet, Text, Image, Button, SafeAreaView } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Whats up ANDis!</Text>
+      <Image
+        source={{
+          width: 300,
+          height: 100,
+
+          uri: "https://commercetools.com/wp-content/uploads/2021/03/and-digital-logo.png",
+        }}
+        resizeMode="contain"
+      />
+      <Button
+        title="Click Me"
+        color="red"
+        onPress={() => alert("Button Clicked")}
+      />
+
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  home: {
+    flex: 1,
+  }
+});
+
+export default HomeScreen;
