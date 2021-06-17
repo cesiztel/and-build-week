aed93053ffe90d6007686b084165e754a6da4dfb
Tom Pope <popetr90@gmail.com>
2021-06-17 10:43:15 +0200

    adding debugger functionality + AND logo

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, Button, SafeAreaView } from 'react-native';
 
 export default function App() {
+ 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Whats up ANDis!</Text>
+      <Image source={{
+        width: 300,
+        height: 100,
+        
+        uri: "https://commercetools.com/wp-content/uploads/2021/03/and-digital-logo.png"}} resizeMode="contain"/>
+        <Button  title="Click Me" color="red" onPress={() => alert("Button Clicked")}/>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
